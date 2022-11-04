36f5c034683f4cc0d9b057bdbcf708af035e9c6e
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-04 08:09:11 +0000

    [add]コメント機能作成途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,31 @@
 class Public::CommentsController < ApplicationController
+
+  def index
+    @tweet    = Tweet.find(params[:tweet_id])
+    @comments = @tweet.comments.all
+  end
+
+  def create
+    tweet            = Tweet.find(params[:tweet_id])
+    comment          = current_user.comments.new(comment_params)
+    comment.tweet_id = tweet.id
+    comment.save
+    redirect_to tweets_path
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to tweet_comments_path(params[:tweet_id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
 end
