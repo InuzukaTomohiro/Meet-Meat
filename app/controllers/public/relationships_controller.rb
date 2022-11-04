b875d2d58470c907b21c80440524fd25c58218ef
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-04 06:48:18 +0000

    [add]フォロー機能

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,2 +1,23 @@
 class Public::RelationshipsController < ApplicationController
+
+  def followings
+    user   = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user   = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+  def create
+    current_user.follow(params[:user_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_back(fallback_location: root_path)
+  end
+
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,6 +2,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tweets = @user.tweets.all
   end
 
   def edit
