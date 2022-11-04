b2753ec07186be6961838ebc7f17d4edc889bc10
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-04 05:38:42 +0000

    [add]いいね機能

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,2 +1,19 @@
 class Public::FavoritesController < ApplicationController
+
+  def index
+    @favorites = Favorite.all
+  end
+
+  def create
+    favorite = Favorite.new(user_id: current_user.id, tweet_id: params[:tweet_id])
+    favorite.save
+    redirect_to tweets_path
+  end
+
+  def destroy
+    favorite = Favorite.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    favorite.destroy
+    redirect_to tweets_path
+  end
+
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -15,4 +15,8 @@ class Tweet < ApplicationRecord
     meat_image.variant(resize_to_limit: [width, height]).processed
   end
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+
 end
