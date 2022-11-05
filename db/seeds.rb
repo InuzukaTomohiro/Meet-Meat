25a7f9b991d553e8d85b3820871998fa82e2c74d
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-05 03:25:02 +0000

    [add]管理者機能途中

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create(
+    email: ENV['SECRET_EMAIL'],
+    password: ENV['SECRET_PASSWORD']
+)
