bd99c0a7eeace61a0aecc8a1d2e04c81ad5ec2ed
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-12 08:51:11 +0000

    [add]通知機能実装完了

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -2,11 +2,11 @@ class Public::NotificationsController < ApplicationController
 
   def index
 
-    @notifications = current_user.active_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+    notifications = current_user.passive_notifications
+    notifications.where(checked: false).each do |notification|
+      notification.update(checked: true)
     end
-    @notification = @notifications.where.not(visitor_id: current_user.id)
+    @notifications = notifications.where.not(visitor_id: current_user.id)
     # @visitor = notification.visitor
     # @visited = notification.visited
   end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,8 +1,9 @@
 class Notification < ApplicationRecord
 
   default_scope -> { order(created_at: :desc) }
-  belongs_to :post,    optional: true
+  belongs_to :tweet,   optional: true
   belongs_to :comment, optional: true
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
+
 end
