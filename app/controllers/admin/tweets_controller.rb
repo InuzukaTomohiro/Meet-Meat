e78b9b94f4d4f07d9845f38137db179253cdc9fb
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-11-29 07:37:25 +0000

    [fix]管理者側ユーザー投稿削除機能修正

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -22,9 +22,12 @@ class Admin::TweetsController < ApplicationController
   # 投稿削除機能、非同期化済み
   def destroy
     @tweets           = Tweet.all.order(created_at: :desc).page(params[:page]).per(10)
+    @tweet = Tweet.find(params[:id])
+    user = @tweet.user
+    @user_tweets      = user.tweets.all.order(created_at: :desc).page(params[:page]).per(5)
     @tweets_all       = Tweet.all
     @no_active_tweets = Tweet.where(is_active: false).order(created_at: :desc).page(params[:page]).per(10)
-    Tweet.find(params[:id]).destroy
+    @tweet.destroy
   end
 
   private
