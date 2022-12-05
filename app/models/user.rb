4036ce366a4abcd716d6b185011e5fe14bd6a0fc
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-12-05 02:43:34 +0000

    [fix]画像表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,8 +22,8 @@ class User < ApplicationRecord
   # プロフィール画像設定
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join("app/asset/images/default-image.jpeg")
-      profile_image.attach(io: File.open(file_path), filename: "default-image.jpg", content_type: "image/jpeg")
+      file_path = Rails.root.join("./app/assets/images/default-image.jpeg")
+      profile_image.attach(io: File.open(file_path), filename: "default-image.jpeg", content_type: "image/jpeg")
     end
     profile_image.variant(resize_to_fill: [width, height]).processed
   end
